adbfbf262c83254954cdc3fde9a5ca0260149ef8
이다은 <llynn97@naver.com>
2022-09-26 18:22:33 +0900

    JPA

--- Spring-DB/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ Spring-DB/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -15,7 +15,7 @@ import javax.sql.DataSource;
 
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
-@Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @Slf4j
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
