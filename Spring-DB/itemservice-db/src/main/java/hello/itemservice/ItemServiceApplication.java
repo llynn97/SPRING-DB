aecadf08085ce615a897d13d28ad0124a200bf12
이다은 <llynn97@naver.com>
2022-09-29 17:43:20 +0900

    Querydsl

--- Spring-DB/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ Spring-DB/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -15,7 +15,7 @@ import javax.sql.DataSource;
 
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
-@Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @Slf4j
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
