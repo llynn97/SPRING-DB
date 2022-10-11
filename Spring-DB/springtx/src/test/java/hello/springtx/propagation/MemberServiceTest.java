968897ac78aa5518dacfc2eed5705fb376c4a194
이다은 <llynn97@naver.com>
2022-10-11 21:44:51 +0900

    Spring Tx Propagation

--- Spring-DB/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ Spring-DB/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -63,7 +63,7 @@ class MemberServiceTest {
     @Test
     void singleTx() {
 
-        String username = "outerTxOff_success";
+        String username = "singleTx";
 
         memberService.joinV1(username);
 
@@ -71,5 +71,73 @@ class MemberServiceTest {
         Assertions.assertTrue(memberRepository.find(username).isPresent());
         Assertions.assertTrue(logRepository.find(username).isPresent());
     }
+
+    /**
+     * MemberService @Transactional:ON
+     * MemberRepository @Transactional:ON
+     * LogRepository @Transactional:ON
+     */
+    @Test
+    void outerTxOn_success() {
+
+        String username = "outerTxOn_success";
+
+        memberService.joinV1(username);
+
+        //모든 데이터가 정상 저장된다
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * MemberService @Transactional:ON
+     * MemberRepository @Transactional:ON
+     * LogRepository @Transactional:ON Exception
+     */
+    @Test
+    void outerTxOn_fail() {
+
+        String username = "로그예외_outerTxOn_fail";
+        org.assertj.core.api.Assertions.assertThatThrownBy(()->memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //모든 데이터가 롤백
+        Assertions.assertTrue(memberRepository.find(username).isEmpty());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * MemberService @Transactional:ON
+     * MemberRepository @Transactional:ON
+     * LogRepository @Transactional:ON Exception
+     */
+    @Test
+    void recoverException_fail() {
+
+        String username = "로그예외_recoverException_fail";
+        org.assertj.core.api.Assertions.assertThatThrownBy(()->memberService.joinV2(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //모든 데이터가 롤백
+        Assertions.assertTrue(memberRepository.find(username).isEmpty());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * MemberService @Transactional:ON
+     * MemberRepository @Transactional:ON
+     * LogRepository @Transactional:ON(REQUIRES NEW) Exception
+     */
+    @Test
+    void recoverException_success() {
+
+        String username = "로그예외_recoverException_success";
+        memberService.joinV2(username);
+
+        //when: member 저장, log 롤백
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+    }
+
 
 }
